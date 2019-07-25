dde7e3c3efdc2e8b31cb4e50f2899e4c703b59a1
daniellequevedo <quevedo.danielle2@gmail.com>
2019-07-24 20:03:22 -0500

    ReportForm DragDrop cleaned up styling

--- src/containers/pages/Reports.js
+++ src/containers/pages/Reports.js
@@ -3,24 +3,19 @@ import React, { useState } from 'react';
 import ReportsList from '../../components/ReportComponents/ReportsList';
 import ReportForm from '../../components/ReportComponents/ReportForm';
 
-import DragDrop from '../../components/ReportComponents/DragDrop';
-
 import '../../styles/pages.css';
 import '../../styles/reports.css';
 
 const Reports = () => {
 
-  const [formMode, setFormMode] = useState(false);
-
-  // 
+  const [formMode, setFormMode] = useState(true);
 
   return (
     <main className='reports-page'>
       { formMode 
-      ? <ReportsList />
-      : <ReportForm />
+      ? <ReportForm />
+      : <ReportsList />
       }
-      <DragDrop />
     </main>
   );
 }
