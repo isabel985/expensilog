a9224cde8b07a6de6b3f4c51d17d02586db1340e
isabel985 <lee.isabel59@gmail.com>
2019-08-01 20:06:12 -0500

    moved state in ReportForm to the parent

--- src/components/ReportComponents/ReportForm.js
+++ src/components/ReportComponents/ReportForm.js
@@ -1,53 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import DragDrop from '../../components/ReportComponents/DragDrop';
 import Avatar from '../Avatar';
 // import { userInfo } from 'os';
-import { user, statements } from '../../utils/data';
 
 const ReportForm = (props) => {
-
-  // move this to reports so we're not having to update state in two different components
-      //  create default name state
-      const [ name, setName ] = useState('');
-
-      useEffect(() => {
-
-      },[props.id]);
-
-      // if we don't have an id
-      //  create a new id
-      let id = Math.floor(Math.random() * 20);
-      let status = 'open';
-
-      // if we have an id
-      // if (props.id) {
-        //  get statement that matches id
-        let foundStatement = statements.find(statement => statement.id === props.id)
-        // if found statement
-        if (foundStatement) {
-          //  override the default variables 
-          console.log('foundStatement is',foundStatement);
-          id = foundStatement.id;
-          setName(foundStatement.name);
-          status = foundStatement.status;
-        // }
-      }
-
-  // create a list of months where the report is open or no report at all
-  let months = ['January 2019', 'February 2019', 'March 2019', 'April 2019', 'May 2019', 'June 2019', 'July 2019', 'August 2019', 'September 2019'];
-  // map over statments and return the statment name
-  let statementMonths = statements.map(statement => statement.name);
-  // filter over months and get back a list of all available months
-  let availableMonths = months.filter(month => !statementMonths.includes(month));
-
-  let monthOptions = availableMonths.map(month => <option>{month}</option>);
-
-  // change title when date is selected
-  const handleOnDateChange = ev => {
-    let selectedDate = ev.target.value;
-    setName(selectedDate);
-  }
-
+  console.log(props);
+  
+  let monthOptions = props.availableMonths.map(month => <option>{month}</option>);
+  
   return (
     <>
       <header>
@@ -64,14 +24,14 @@ const ReportForm = (props) => {
 
           <div className='report-header'>
             <div className='status-id-wrapper'>
-              <div className={`status-label status`}>{status}</div>
-              <div className='report-id'><span>ID:</span>{id}</div>
+              <div className={`status-label status`}>{props.status}</div>
+              <div className='report-id'><span>ID:</span>{props.id}</div>
 
             </div>
 
             <div className='form-control'>
               <label>Report</label>
-              <select onChange={handleOnDateChange}>
+              <select onChange={props.handleOnDateChange}>
                 <option>Select Date</option>
                 {monthOptions}
               </select>
@@ -79,15 +39,15 @@ const ReportForm = (props) => {
           </div>
 
           <div className='report-header'>
-            <div className='report-name'>Expense Report {name}</div>
+            <div className='report-name'>Expense Report {props.name}</div>
           </div>
 
           <div>
             <div>
               <DragDrop>
                 <div className='report-from-to'>
-                  <div className=''><Avatar img={user.avatarImg} />From: <span>{user.name}</span></div>
-                  <div className=''><Avatar img={user.supervisor.avatarImg} />To: <span>{user.supervisor.name}</span></div>
+                  <div className=''><Avatar img={props.user.avatarImg} />From: <span>{props.user.name}</span></div>
+                  <div className=''><Avatar img={props.user.supervisor.avatarImg} />To: <span>{props.user.supervisor.name}</span></div>
                 </div>
               </DragDrop>
             </div>
--- src/containers/pages/Reports.js
+++ src/containers/pages/Reports.js
@@ -1,20 +1,44 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { user, statements } from '../../utils/data';
 import ReportsList from '../../components/ReportComponents/ReportsList';
 import ReportForm from '../../components/ReportComponents/ReportForm';
-
 import '../../styles/pages.css';
 import '../../styles/reports.css';
 
 const Reports = () => {
   const [formMode, setFormMode] = useState(false);
-  const [formId, setFormId] = useState(null);
+  const [formId, setFormId] = useState(Math.floor(Math.random() * 20));
+  const [name, setName] = useState('');
+  // useEffect(()=>{},[name]);
+  let status = 'open';
+
+  // create a list of months where the report is open or no report at all
+  let months = ['January 2019', 'February 2019', 'March 2019', 'April 2019', 'May 2019', 'June 2019', 'July 2019', 'August 2019', 'September 2019'];
+  // map over statments and return the statment name
+  let statementMonths = statements.map(statement => statement.name);
+  // filter over months and get back a list of all available months
+  let availableMonths = months.filter(month => !statementMonths.includes(month));
+
+  // change title when date is selected
+  const handleOnDateChange = ev => {
+    let selectedDate = ev.target.value;
+    setName(selectedDate);
+  }
 
   const onReportClick = (id) => {
     console.log('onReportClick was called');
     console.log('id is ', id);
-    setFormMode(true);
-    setFormId(id);
+    let foundStatement = statements.find(statement => statement.id === id)
+    // if found statement
+    if (foundStatement) {
+      // override the default variables 
+      console.log('foundStatement is', foundStatement);
+      setFormId(foundStatement.id);
+      setName(foundStatement.name);
+      status = foundStatement.status;
+      setFormMode(true);
+    }
+    alert('no statement found');
   }
 
   const newReportClick = () => {
@@ -24,9 +48,20 @@ const Reports = () => {
 
   return (
     <main className='reports-page'>
-      { formMode 
-      ? <ReportForm id={formId} />
-      : <ReportsList onReportClick={onReportClick} newReportClick={newReportClick} />
+      {formMode
+        ? <ReportForm
+          id={formId}
+          setName={setName}
+          name={name}
+          user={user}
+          status={status}
+          availableMonths={availableMonths}
+          handleOnDateChange={handleOnDateChange}
+        />
+        : <ReportsList
+          onReportClick={onReportClick}
+          newReportClick={newReportClick}
+        />
       }
     </main>
   );
